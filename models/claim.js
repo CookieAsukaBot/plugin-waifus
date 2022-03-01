0aa5b88790897e69db6607c2e4e0e3eaf3b8e7aa
RikkaHasCookies <imhunter_@outlook.com>
2022-03-01 12:55:25 -0800

    model

---
+++ models/claim.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const schema = new Schema({
+    id: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    guild: {
+        type: String,
+        required: true
+    },
+    user: {
+        id: {
+            type: String,
+            required: true
+        },
+        position: {
+            type: Number,
+            required: true
+        },
+        tags: [{
+            type: String
+        }]
+    },
+    metadata: {
+        domain: {
+            type: String
+        },
+        id: {
+            type: String
+        },
+        type: {
+            type: String, // ART, CHARACTER
+            required: true
+        }
+    },
+    character: {
+        name: {
+            type: String
+        },
+        gender: {
+            type: Number // 0 female, 1 male, 2 non-binary
+        },
+        anime: {
+            type: String
+        },
+        url: {
+            type: String
+        }
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = model('waifus_claim', schema);
