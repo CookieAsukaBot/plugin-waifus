7b2f88817a8d4811d2dbcbd2cf9bb206b8aedc05
RikkaHasCookies <imhunter_@outlook.com>
2022-03-09 08:54:56 -0800

    se agregó claim, mejor debugging info y asyncs faltantes

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,3 +1,5 @@
+const { nanoid } = require('nanoid');
+const status = require('../helpers/status');
 const User = require('../models/user');
 const Claim = require('../models/claim');
 
@@ -8,7 +10,7 @@ const Claim = require('../models/claim');
  * @param {String} userID ID del usuario a crear.
  * @returns {Object} retorna el usuario creado o encontrado.
  */
-const getUser = (guild, userID) => {
+const getUser = async (guild, userID) => {
     let data = { guild, id: userID };
     try {
         let user = await User.find(data);
@@ -16,7 +18,7 @@ const getUser = (guild, userID) => {
         return user;
     } catch (error) {
         console.log(error);
-        return false;
+        return status.failed("DB_ERROR");
     };
 };
 
@@ -26,16 +28,74 @@ const getUser = (guild, userID) => {
  * @param {String} guild ID del servidor.
  * @param {String} userID ID del usuario a encontrar.
  * @param {String} order Elegir el orden a devolver (SIGUE WIP).
- * @returns {Array}? retorna todas las reclamaciones encontradas.
+ * @returns {Array} retorna todas las reclamaciones encontradas.
  */
-const getHarem = (guild, userID, order) => {
+const getHarem = async (guild, userID, order) => {
     try {
         let harem = await Claim.find({ guild, "user.id": userID })
             .sort(order || { updatedAt: -1 });
         return harem;
     } catch (error) {
         console.log(error);
-        return false;
+        return status.failed("DB_ERROR");
+    };
+};
+
+/**
+ * Se comprueba la existencia del usuario, después se cambia el estado de reclamación a falso, se crea el módelo a guardar, se comprueban campos extras y guarda el módelo.
+ * 
+ * @param {String} guild ID del servidor.
+ * @param {String} userID ID del usuario a encontrar y guardar.
+ * @param {Object} data objeto
+ * @returns retorna un mensaje de éxito.
+ */
+const claim = async (guild, userID, data) => {
+    let { anime, name, gender } = data.image;
+
+    try {
+        let user = await getUser(guild, userID);
+        if (user.fun.canClaim == false) return status.failed(`USER_CANT_CLAIM`);
+
+        await User.updateOne({ id: userID }, {
+            "fun.canClaim": false
+        });
+
+        let claimed = new Claim({
+            id: nanoid(12),
+            guild,
+            user: {
+                id: userID,
+                position: user.stats.count || 1
+            },
+            metadata: {
+                domain: data.image.domain,
+                id: data.image.id,
+                type: data.image.type,
+                url: data.image.url
+            }
+        });
+
+        if (anime) claimed.character.anime = anime;
+        if (name) claimed.character.name = name;
+        if (gender) {
+            switch (gender) {
+                case "Male":
+                    claimed.character.gender = 1;
+                    break;
+                case "Non-binary":
+                    claimed.character.gender = 2;
+
+                default:
+                    claimed.character.gender = 0;
+                    break;
+            };
+        };
+
+        await claimed.save();
+        return status.success("SUCCESS", claimed);
+    } catch (error) {
+        console.log(error);
+        return status.failed(`DB_ERROR`);
     };
 };
 
@@ -44,7 +104,7 @@ const getHarem = (guild, userID, order) => {
  * @param {String} userID ID del usuario para hacer el cambio.
  * @param {String} newData Título nuevo/actualizado a mostrar.
  */
-const changeHaremTitle = (guild, userID, newData) => {
+const changeHaremTitle = async (guild, userID, newData) => {
     try {
         await User.updateOne({ guild, userID}, {
             "harem.title": newData
@@ -52,7 +112,7 @@ const changeHaremTitle = (guild, userID, newData) => {
         return true;
     } catch (error) {
         console.log(error);
-        return false;
+        return status.failed("DB_ERROR");
     };
 };
 
@@ -61,7 +121,7 @@ const changeHaremTitle = (guild, userID, newData) => {
  * @param {String} userID ID del usuario para hacer el cambio.
  * @param {String} newData Color nuevo/actualizado a mostrar.
  */
- const changeHaremColor = (guild, userID, newData) => {
+ const changeHaremColor = async (guild, userID, newData) => {
     try {
         await User.updateOne({ guild, userID}, {
             "harem.color": newData
@@ -69,13 +129,14 @@ const changeHaremTitle = (guild, userID, newData) => {
         return true;
     } catch (error) {
         console.log(error);
-        return false;
+        return status.failed("DB_ERROR");
     };
 };
 
 module.exports = {
     getUser,
     getHarem,
+    claim,
     changeHaremTitle,
     changeHaremColor
 };
