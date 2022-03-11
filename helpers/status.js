a9ccc1a1fb7ac72f4880cf152b41d52a182dba6d
RikkaHasCookies <imhunter_@outlook.com>
2022-03-11 09:32:38 -0800

    documentación

--- helpers/status.js
+++ helpers/status.js
@@ -1,3 +1,10 @@
+/**
+ * Se usa para mostrar que la acción tuvo éxito.
+ * 
+ * @param {String} message mensaje a mostrar.
+ * @param {*} data puede ser cualquier cosa, desde un String a un Objeto.
+ * @returns retorna un objeto con el status en true (de éxito), un mensaje y los datos.
+ */
 const success = (message, data) => {
     return {
         status: true,
@@ -6,6 +13,12 @@ const success = (message, data) => {
     }
 };
 
+/**
+ * Se usa para mostrar que la acción no tuvo éxito.
+ * 
+ * @param {String} message mensaje a mostrar.
+ * @returns retorna un objeto con el status en false (por el error) y un mensaje.
+ */
 const failed = (message) => {
     return {
         status: false,
