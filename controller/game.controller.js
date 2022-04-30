e572ae759e7e1d03283a5b62ea40061553ec33e1
RikkaHasCookies <imhunter_@outlook.com>
2022-04-29 20:08:20 -0700

    manipulación y comprobación de rolls

--- controller/game.controller.js
+++ controller/game.controller.js
@@ -1,8 +1,45 @@
 const status = require('../helpers/status');
+const User = require('../models/user');
+const { getUser } = require('../controller/user.controller');
 const { getRandomNumber } = require('../utils/random-things');
 
 const anilist = require('../api/anilist');
 const booru = require('../api/booru');
+
+/**
+ * Comprueba si el usuario existe y puede tirar.
+ * Actualiza el usuario (su estadística y rolls disponibles).
+ * También retorna un mensaje si al jugador le quedan 2 rolls disponibles.
+ * 
+ * @param {String} guild ID del servidor.
+ * @param {String} userID ID del usuario.
+ */
+const userCanRoll = async (guild, userID) => {
+    let player = await getUser(guild, userID);
+    if (player.status == false) return status.failed("Ocurrió un error al encontrar tu usuario.");
+    player = player.data[0];
+    
+    try {
+        if (player.fun.rolls < 1) return status.failed(`<@${userID}>, no tienes rolls disponibles!\nEl reinicio es [placeholder].`);
+
+        await User.updateOne({
+            guild,
+            id: userID
+        }, {
+            $inc: {
+                "fun.rolls": -1,
+                "stats.rolls.count": 1
+            }
+        });
+
+        let message = "";
+        if (player.fun.rolls == 3) message = `⚠ quedan **2** rolls ⚠`;
+        return status.success(message, null);
+    } catch (error) {
+        console.error(error);
+        status.failed("DB_ERROR");
+    };
+};
 
 /**
  * Genera un número aleatorio, después base al número se elige una función de una API.
@@ -26,6 +63,8 @@ const getRandomRoll = async (guild) => {
                 break;
         };
 
+        // ¿Comprobar si tiene dueño?
+
         // WIP: ¿comprobaciones de error?
         return status.success("SUCCESS", model.data);
     } catch (error) {
@@ -35,5 +74,6 @@ const getRandomRoll = async (guild) => {
 };
 
 module.exports = {
-    getRandomRoll
+    userCanRoll,
+    getRandomRoll,
 };
