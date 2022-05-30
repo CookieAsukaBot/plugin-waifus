4f5ecd74a3bdf29f67d01276cd83cc1adc3e5026
RikkaHasCookies <imhunter_@outlook.com>
2022-05-30 02:07:05 -0700

    sort de harem deshabilitado temporalmente, necesita planeación

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -36,7 +36,7 @@ const getUser = async (guild, userID) => {
 const getHarem = async (guild, userID, order) => {
     try {
         let harem = await Claim.find({ guild, "user.id": userID })
-            .sort(order || { updatedAt: -1 });
+            // .sort(order || { updatedAt: -1 });
         return status.success("SUCCESS", harem);
     } catch (error) {
         console.error(error);
