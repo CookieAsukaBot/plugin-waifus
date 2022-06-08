9e4d58f4f4303f8d87fa9c1e93d43cc4600b27f7
RikkaHasCookies <imhunter_@outlook.com>
2022-06-07 23:09:59 -0700

    human mistake
    se encontraba como populate para hacer referencia en el futuro

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const schema = new Schema({
     },
     harem: {
         favourites: [{
-            type: populate
+            type: Schema.ObjectId
         }],
         order: {
             by: {
