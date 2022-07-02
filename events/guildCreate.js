014a6320ddeb04393be95f94a2612db666c5ba85
RikkaHasCookies <imhunter_@outlook.com>
2022-07-02 12:18:24 -0700

    los contadores no funcionaban al unirse

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,6 +2,6 @@ const { setupGuild } = require('../helpers/update-users');
 
 module.exports = (bot) => {
     bot.on('guildCreate', async guild => {
-        setupGuild(guild, bot.readyAt);
+        await setupGuild(guild, bot.readyAt);
     });
 };
